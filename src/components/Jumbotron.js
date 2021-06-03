50b5f6a50b1bf7b4cf791c7918d85abe84bdcea4
Brendan McCaffrey <brendanchristophermccaffrey@gmail.com>
2021-06-03 18:40:34 -0400

    banner progress

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Jumbotron as Jumbo, Container } from 'react-bootstrap';
 import styled from 'styled-components';
 
+import logo from '../assets/logo-0-white-highres.png';
+
 
 const Styles = styled.div`
-    .jumbotron {
+    .jumbo {
         background: #000;
         height: 78vh;
         margin-left: 4vw;
@@ -12,16 +14,64 @@ const Styles = styled.div`
         margin-top: 17vh;
         // position: relative;
         margin-bottom: 10vh;
-        align-text: center;
+        textAlign: center;
+    }
+
+    .jumbo2 {
+        background: #fff;
+    }
+
+    #mycenter {
+        text-align: center;
+    }
+
+    #bannerlogo {
+        width: 10vw;
+        z-index: 30;
+        margin-top: 7vh;
+    }
+
+    #bannertext {
+        flex-direction: row;
+        flexShrink: 1;
+        color: #fff;
+        font-family: 'Spectral', sans-serif;
+        font-size: 3em;
+    }
+
+    #bannersubtext {
+        flex-direction: row;
+        flexShrink: 1;
+        color: #fff;
+        font-family: 'Noto Sans 400 Italic 400', sans-serif;
+        font-size: 1em;
+        font-style: italic;
+        font-weight: 100;
     }
 `;
 
 export const Jumbotron = () => (
     <Styles>
-        <Jumbo fluid classname="jumbo">
-            <div classname="overlay"></div>
-            <Container>
-                Hello World
+        <Jumbo fluid className="jumbo">
+            {/* <div className="overlay"> */}
+                {/* <Jumbo fluid className="jumbo2">
+                    <Container>
+                        Yup
+                    </Container>
+                </Jumbo> */}
+            {/* </div> */}
+            <Container >
+                <div id="mycenter">
+                    <img id="bannerlogo" src={logo} alt="Undefined"/>
+                </div>
+                <div id="mycenter">
+                    <p id="bannertext">
+                        Our mandate is flexible, limitless, and simply, UNDEFINED.
+                    </p>
+                    <p id="bannersubtext">
+                        We are cross-asset, stage-agnostic investors applying creativity and unique insights to generate alpha accross the globe.
+                    </p>
+                </div>
             </Container>
         </Jumbo>
     </Styles>
